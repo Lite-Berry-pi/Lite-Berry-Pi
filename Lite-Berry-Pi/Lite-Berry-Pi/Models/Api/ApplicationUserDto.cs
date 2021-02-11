6b2eca4a3dec1b94845ff2dad4e06ffebfa8d518
Scott Falbo <scottfalboart@gmail.com>
2021-02-10 16:19:32 -0800

    few small tidy ups and docs started

--- Lite-Berry-Pi/Lite-Berry-Pi/Models/Api/ApplicationUserDto.cs
+++ Lite-Berry-Pi/Lite-Berry-Pi/Models/Api/ApplicationUserDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace Lite_Berry_Pi.Models.Api
 {
     public class ApplicationUserDto
     {
-        public string Id { get; set; }
+        [Required]
+        public int Id { get; set; }
+        [Required]
         public string Username { get; set; }
 
         public string Token { get; set; }
