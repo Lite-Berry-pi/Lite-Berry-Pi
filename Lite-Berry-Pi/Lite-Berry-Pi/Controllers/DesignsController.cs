9d1cec27286ccd567ca0fb0aad4969d8d3331da4
Scott Falbo <scottfalboart@gmail.com>
2021-02-12 10:55:56 -0800

    server log off issue

--- Lite-Berry-Pi/Lite-Berry-Pi/Controllers/DesignsController.cs
+++ Lite-Berry-Pi/Lite-Berry-Pi/Controllers/DesignsController.cs
@@ -45,7 +45,8 @@ namespace Lite_Berry_Pi.Controllers
             }
             return design;
         }
-        [Authorize(Roles = "Administrator")]
+        //[Authorize(Roles = "Administrator")]
+        [AllowAnonymous]
         // PUT: api/Designs/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
